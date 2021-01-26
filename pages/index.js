96dd507d7ee5c03f03907cea0fc3b010360a9893
lihbr <lihbr@users.noreply.github.com>
2021-01-26 14:59:40 +0100

    fix(core): next build

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,15 @@
 import { Client } from "../prismicKits";
 import SliceZone from "next-slicezone";
-import { useGetStaticProps, useGetStaticPaths } from "next-slicezone/hooks";
+import { useGetStaticProps } from "next-slicezone/hooks";
 
 import resolver from "../sm-resolver.js";
 
 const Page = (props) => <SliceZone {...props} resolver={resolver} />;
 
-
 // Fetch content from prismic
 export const getStaticProps = useGetStaticProps({
   client: Client(),
-  uid: () => "home"
-});
-
-export const getStaticPaths = useGetStaticPaths({
-  client: Client(),
-  type: "page",
-  fallback: true, // process.env.NODE_ENV === "development",
-  formatPath: () => ({ params: { uid: "home" }})
+  uid: () => "home",
 });
 
 export default Page;
